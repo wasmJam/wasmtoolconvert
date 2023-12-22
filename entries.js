bde269f73d95871cf2fe1063e4a657079202a769
sangam14 <smbiradar14@gmail.com>
2023-12-23 01:14:22 +0530

    add

--- entries.js
+++ entries.js
@@ -1,10 +1,17 @@
 const entries = [
   {
-    "title": "emscripten-core",
+    "title": "assemblyscript",
     "link": NaN,
-    "description": "Emscripten: An LLVM-to-WebAssembly Compiler",
+    "description": "A TypeScript-like language for WebAssembly",
     "tag": "compiler",
-    "github": "emscripten-core/emscripten"
+    "github": "AssemblyScript/assemblyscript"
+  },
+  {
+    "title": "Bytecoder",
+    "link": NaN,
+    "description": "Framework to interpret and transpile JVM bytecode to JavaScript, OpenCL or WebAssembly.",
+    "tag": "compiler",
+    "github": "WebAssembly/binaryen"
   },
   {
     "title": "binaryen",
@@ -14,6 +21,27 @@ const entries = [
     "github": "WebAssembly/binaryen"
   },
   {
+    "title": "cheerp-compiler",
+    "link": NaN,
+    "description": "C++ to JavaScript/WebAssembly compiler",
+    "tag": "compiler",
+    "github": "leaningtech/cheerp-compiler"
+  },
+  {
+    "title": "emscripten-core",
+    "link": NaN,
+    "description": "Emscripten: An LLVM-to-WebAssembly Compiler",
+    "tag": "compiler",
+    "github": "emscripten-core/emscripten"
+  },
+  {
+    "title": "JWebAssembly",
+    "link": NaN,
+    "description": "Java bytecode to WebAssembly compiler",
+    "tag": "compiler",
+    "github": "i-net-software/JWebAssembly"
+  },
+  {
     "title": "tinygo",
     "link": NaN,
     "description": "Go compiler for small places. Microcontrollers, WebAssembly (WASM/WASI), and command-line tools. Based on LLVM.",
@@ -21,17 +49,31 @@ const entries = [
     "github": "tinygo-org/tinygo"
   },
   {
-    "title": "Bytecoder",
+    "title": "waforth",
     "link": NaN,
-    "description": "Framework to interpret and transpile JVM bytecode to JavaScript, OpenCL or WebAssembly.",
+    "description": "Small but complete dynamic Forth Interpreter/Compiler for and in WebAssembly",
     "tag": "compiler",
-    "github": "mirkosertic/Bytecoder"
+    "github": "remko/waforth"
   },
   {
-    "title": "assemblyscript",
+    "title": "wasmtime",
     "link": NaN,
-    "description": "A TypeScript-like language for WebAssembly",
-    "tag": "compiler",
-    "github": "AssemblyScript/assemblyscript"
+    "description": "WebAssembly Profiler based on Wazero",
+    "tag": "profiler",
+    "github": "bytecodealliance/wasmtime"
+  },
+  {
+    "title": "wasm-score",
+    "link": NaN,
+    "description": "A benchmark for standalone WebAssembly",
+    "tag": "benchmark ",
+    "github": "bytecodealliance/wasm-score"
+  },
+  {
+    "title": "wabt",
+    "link": NaN,
+    "description": "The WebAssembly Binary Toolkit",
+    "tag": "binary",
+    "github": "WebAssembly/wabt"
   }
 ];
